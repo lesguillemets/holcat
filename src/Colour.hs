761f6022bc29075fa92c36fe6e8a587c9f256aad
lesguillemets <lesguillemets@users.noreply.github.com>
2015-08-08 02:06:27 +0900

    256-basic

--- src/Colour.hs
+++ src/Colour.hs
@@ -1,5 +1,21 @@
 {-# LANGUAGE OverloadedStrings #-}
+import Data.List
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 
-main = TIO.putStrLn "こんにちは!"
+data Colour = RGB {_r :: Int, _g :: Int, _b :: Int}
+
+data Hi = Hi256 { _fg :: Colour, _bg :: Colour }
+        | None
+
+toSeq :: Hi -> T.Text
+toSeq None = T.pack "\027[0m"
+toSeq (Hi256 (RGB fr fg fb) (RGB br bg bb)) = T.pack $
+    "\027[38;2;" ++ (intercalate ";" . map show) [fr,fg,fb] ++ "m" ++
+    "\027[48;2;" ++ (intercalate ";" . map show) [br,bg,bb] ++ "m"
+
+main = do
+    TIO.putStr . toSeq $ Hi256 (RGB 0 243 234) (RGB 34 123 0)
+    TIO.putStr "こんにちは!"
+    TIO.putStrLn . toSeq $ None
+    TIO.putStrLn "こんにちは!"
