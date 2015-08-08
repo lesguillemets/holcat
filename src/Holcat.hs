9f1608136e607db6fdeef1383fbe62dc43297d3b
lesguillemets <lesguillemets@users.noreply.github.com>
2015-08-09 02:25:12 +0900

    add options

--- src/Holcat.hs
+++ src/Holcat.hs
@@ -28,14 +28,28 @@ holcat (Config dx dy s v style) txt =
             clearHl
             putStrLn ""
 
-holcatMain :: Flag "r" '["rainbow"] "bg/fg" "bg/fg" (Def "fg" String)
-           -> Arg "filename" String
-           -> Cmd "holcat!" ()
-holcatMain bf fName = liftIO $
+holcatMain ::
+       Flag "r" '["rainbow"] "bg/fg" "bg/fg" (Def "fg" String)
+    -> Flag "x" '["dx"] "Double" "dhue/dx default:5" (Def "5" Double)
+    -> Flag "y" '["dy"] "Double" "dhue/dy default:5" (Def "5" Double)
+    -> Flag "V" '["value"] "Double <-[0.1]" "value" (Def "1" Double)
+    -> Flag "s" '["saturation"] "Double <-[0.1]" "saturation" (Def "1" Double)
+    -> Arg "filename" String
+    -> Cmd "holcat!" ()
+-- TODO : want to use v
+-- TODO : handle no filename specified
+-- TODO : sanitise
+holcatMain bf dx dy v s fName = liftIO $
     TIO.readFile (get fName) >>= holcat config
     where
     config = case get bf of
-        "bg" -> defaultBGConfig
-        _ -> defaultConfig
+        "bg" -> defaultBGConfig {
+                dhdx = get dx, dhdy = get dy,
+                value = get v, saturation = get s
+                }
+        _ -> defaultConfig {
+                dhdx = get dx, dhdy = get dy,
+                value = get v, saturation = get s
+                }
 
 main = run_ holcatMain
